903cb7339955623307c1faa3cd3894ab86d42289
Dmitri <unabl4@gmail.com>
2019-10-25 00:29:55 +0300

    challenge 8 (person grouper) solution

--- src/main/java/ee/itcollage/level8/PersonGrouper.java
+++ src/main/java/ee/itcollage/level8/PersonGrouper.java
@@ -1,14 +1,23 @@
 package ee.itcollage.level8;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class PersonGrouper {
 
     //todo fix tests
     public static Map<String, Person> groupPeopleByIdCode(List<Person> people){
-        return new HashMap<>();
+        Map<String, List<Person>> h = people.stream().collect(groupingBy(Person::getIdCode));
+
+        return h.entrySet().stream().collect(
+            Collectors.toMap(
+                Map.Entry::getKey,
+                e -> e.getValue().get(0)
+            )
+        );
     }
 
 }
