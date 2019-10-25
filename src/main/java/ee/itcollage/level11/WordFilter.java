2514a26ef03b6860b478210f25fb2b0c6cb6611e
Dmitri <unabl4@gmail.com>
2019-10-25 23:47:12 +0300

    challenge 11 (word filter) solution

--- src/main/java/ee/itcollage/level11/WordFilter.java
+++ src/main/java/ee/itcollage/level11/WordFilter.java
@@ -1,37 +1,37 @@
 package ee.itcollage.level11;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WordFilter {
 
     //todo fix tests
 
     public List<Word> getNouns(List<Word> words) {
-        return words;
+        return words.stream().filter(w -> w.getWordType() == Word.WordType.NOUN).collect(Collectors.toList());
     }
 
     public Word getFirstVerb(List<Word> words) {
-        return words.get(0);
+        return getFirstOfType(words, Word.WordType.VERB);   // re-use
     }
 
     public Word getFirstOfType(List<Word> words, Word.WordType type) {
-        return words.get(0);
+        return words.stream().filter(w -> w.getWordType() == type).findFirst().get();
     }
 
     public List<Word> getAllNotNouns(List<Word> words) {
-        return words;
+        return words.stream().filter(w -> w.getWordType() != Word.WordType.NOUN).collect(Collectors.toList());
     }
 
     public List<String> getNounStrings(List<Word> words){
-        return new ArrayList<>();
+        return getNouns(words).stream().map(Word::getWord).collect(Collectors.toList());
     }
 
     public String getFirstVerbString(List<Word> words) {
-        return "";
+        return getFirstVerb(words).getWord();
     }
 
     public String getFirstStringOfType(List<Word> words, Word.WordType type) {
-        return "";
+        return getFirstOfType(words, type).getWord();
     }
 }
